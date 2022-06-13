4faf28ff287c212483c5f2f0ba4ab05f6635ae3d
Goitom Kassaye <japu111@yahoo.com>
2022-06-13 13:20:28 -0700

    fixed JS files for product upload

--- Javascript/itemController.js
+++ Javascript/itemController.js
@@ -1,7 +1,7 @@
 class ItemsController {
-    constructor(currentId = 0){
-        this.items = [];
-        this.currentId = currentId;
+    constructor(){
+        this.items = localitems;
+        this.currentId = localId + 1;
     } 
     addItem(name, category, subCategory, price, description, picture){
         const item = {
@@ -15,4 +15,16 @@ class ItemsController {
         }
         this.items.push(item);
     }
+    setitems(localitems){
+        this.items = localitems
+        }
+    }
+let localitems = []
+let localId;
+if(localStorage.getItem('product')) {
+    localitems = JSON.parse(localStorage.getItem('product'))
+    localId = localitems[localitems.length - 1].id
+} else {
+    localId = 0
 }
+
